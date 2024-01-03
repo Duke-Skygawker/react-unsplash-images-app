19e82764c0c921655ae87abcf32f08525c2cfc7a
Duke-Skygawker <bart3kk990@gmail.com>
2024-01-03 16:51:26 +0100

    search form styled

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const [value, setValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+    setValue("");
+  };
+  return (
+    <div>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="form-input search-input"
+          type="text"
+          name="search"
+          placeholder="cat"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 export default SearchForm;
